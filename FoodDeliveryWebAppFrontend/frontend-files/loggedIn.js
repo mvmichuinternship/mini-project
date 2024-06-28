5597df513cd48cd3813cf0d4fb0dc752531feca5
mv <mrramidua@gmail.com>
2024-06-29 01:28:46 +0530

    Added toasts, removed logs

--- FoodDeliveryWebAppFrontend/frontend-files/loggedIn.js
+++ FoodDeliveryWebAppFrontend/frontend-files/loggedIn.js
@@ -15,12 +15,35 @@ const checkLoggedIn=()=>{
 
   const tokenValidation=()=>{
     var token = localStorage.getItem("token");
-    if(token==undefined || !token){
+    // var role = localStorage.getItem("role");
+    if(token==="undefined" || !token){
         window.location.href="./login.html"
+    }
+  }
+  const checkAdmin=()=>{
+    var role = localStorage.getItem("role");
+    var token = localStorage.getItem("token");
+    var id= document.getElementById("admin-dash-menu")
+    if(role!="Admin" || token==undefined || !token){
+      showToast("Unauthorised entry not permitted",1000,'#de0a26')
+      id.classList.add("hidden")
+      setTimeout(()=>window.location.href="./home.html", 1000);
     }
   }
 
   const logout=()=>{
     localStorage.clear()
     window.location.reload()
+  }
+
+  function showToast(message, duration = 1000, color) {
+    console.log("Toasted")
+    Toastify({
+      text: message,
+      duration: duration,
+      gravity: 'top', 
+      position: 'center', 
+      close: true ,
+      backgroundColor: color
+    }).showToast();
   }
